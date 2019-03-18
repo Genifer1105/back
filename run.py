f3b33f15bcfa34a742133f459ce94a6439f9e2da
Genifer Tatiana Moreno Morales <genifer1105@gmail.com>
2019-03-18 18:50:53 -0500

    Versión 18 mar

--- run.py
+++ run.py
@@ -2,7 +2,7 @@ from app import Application
 
 def run():
     app = Application.create_app()
-    app.run(debug=True, host='0.0.0.0')
+    app.run(debug=True)
 
 if __name__ == '__main__':
     run()
