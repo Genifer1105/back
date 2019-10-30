d72160dd61602c1ab3da130fb778f2c7b70215e8
David Castro <dcastro@virtualbeams.com>
2019-10-29 21:15:09 -0500

    se cambia la url de la bd

--- app/config/config.py
+++ app/config/config.py
@@ -1,3 +1,3 @@
 class Configuration():
-    SQLALCHEMY_DATABASE_URI = 'mysql+mysqlconnector://root:chuck norris@localhost/ppi'
+    SQLALCHEMY_DATABASE_URI = 'mysql+mysqlconnector://root:1234@localhost/ppi'
     SQLALCHEMY_TRACK_MODIFICATIONS = False
