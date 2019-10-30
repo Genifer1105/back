07f008e83cdb18d7fc13552ba8cc307a9ebddf39
David Castro <dcastro@virtualbeams.com>
2019-10-30 12:23:55 -0500

    fix bd scheme

--- app/config/config.py
+++ app/config/config.py
@@ -1,3 +1,3 @@
 class Configuration():
-    SQLALCHEMY_DATABASE_URI = 'mysql+mysqlconnector://root:1234@localhost/ppi'
+    SQLALCHEMY_DATABASE_URI = 'mysql+mysqlconnector://root:1234@localhost/PPI'
     SQLALCHEMY_TRACK_MODIFICATIONS = False
