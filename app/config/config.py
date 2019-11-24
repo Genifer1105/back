c9a4ebc3d3f2ecfb46cc866f3e75fe12746c0a2c
David Castro <dcastro@virtualbeams.com>
2019-11-24 12:02:20 -0500

    fix alquemy squeme uri

--- app/config/config.py
+++ app/config/config.py
@@ -1,7 +1,7 @@
 from datetime import timedelta
 
 class Configuration():
-    SQLALCHEMY_DATABASE_URI = 'mysql+mysqlconnector://root:chuck norris@localhost/PPI'
+    SQLALCHEMY_DATABASE_URI = 'mysql+mysqlconnector://PPI:ppi123@localhost/PPI'
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     JWT_SECRET_KEY = '08F795D0AE8EDA93F76B2943F2825014A31A6601F352966772C71416912B8CA7'
     JWT_ACCESS_TOKEN_EXPIRES = timedelta(days=30)
